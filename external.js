829512a270655fd1902b1597a20a3894efa3fef2
Taylor Barinka <barinkataylor@gmail.com>
2022-07-13 12:13:36 -0400

    Add a runnign score tally for player & computer

--- external.js
+++ external.js
@@ -1,24 +1,30 @@
 
+const results = document.querySelector('#round-results');
+const tallyC = document.querySelector('#tally-comp');
+const tallyP = document.querySelector('#tally-play');
+
 window.onload=function() {
 
     //button variables
-    const rock = document.querySelector('#rock');
-    const paper = document.querySelector('#paper');
-    const scissors = document.querySelector('#scissors');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 
     //other variables
-    var results = document.querySelector('#display-results');
+const results = document.querySelector('#round-results');
+
+
     
     //button event listeners
-    rock.addEventListener('click', ()=>  {
-        playRound('rock');
-    });
-    paper.addEventListener('click', ()=>  {
-       playRound('paper');
-    });
-    scissors.addEventListener('click', ()=>  {
-        playRound('scissors');
-    });
+rock.addEventListener('click', ()=>  {
+    playRound('rock');
+ });
+paper.addEventListener('click', ()=>  {
+    playRound('paper');
+});
+scissors.addEventListener('click', ()=>  {
+    playRound('scissors');
+});
 
 
 function computerPlay() {
@@ -32,31 +38,41 @@ function computerPlay() {
     }
 }
 
+//Display the running score, and announce a winner of the game once one player reaches 5 points.
+
+//1X. Create a loop that plays five rounds.
+//2. At the end of each round, print the result using this formula: [element].textContent += "<added text>";
+//3. At the end of five rounds, print the winner. Print "Play again?" vel sim.
+//4. Reset after five turns.
+
+
+
 function playRound(variable) {
     let playerSelectionLowerCase = variable.toLowerCase();
     let computerSelectionLowerCase = computerPlay().toLowerCase();
     
     if ((playerSelectionLowerCase == "rock") && (computerSelectionLowerCase == "paper")) {
-        return results.textContent = "You lose! Paper beats rock!";
+        results.textContent = "You lose! Paper beats rock!", tallyC.textContent += "I";
     } else if ((playerSelectionLowerCase == "rock") && (computerSelectionLowerCase == "rock")) {
-        return results.textContent = "A draw! Try again!";
+        results.textContent = "A draw! Try again!";
     } else if ((playerSelectionLowerCase == "rock") && (computerSelectionLowerCase == "scissors")) {
-        return results.textContent = "You win! Rock beats Scissors!";
+        results.textContent = "You win! Rock beats Scissors!", tallyP.textContent += "I";
     } else if ((playerSelectionLowerCase == "paper") && (computerSelectionLowerCase == "paper")) {
-       return  results.textContent = "A draw! Try again!";
+        results.textContent = "A draw! Try again!"
     } else if ((playerSelectionLowerCase == "paper") && (computerSelectionLowerCase == "rock")) {
-        return results.textContent = "You win! Paper beats rock!";
+        results.textContent = "You win! Paper beats rock!", tallyP.textContent += "I";
     } else if ((playerSelectionLowerCase == "paper") && (computerSelectionLowerCase == "scissors")) {
-        return results.textContent = "You lose! Scissors beat paper!";
+        results.textContent = "You lose! Scissors beat paper!", tallyC.textContent += "I"
     } else if ((playerSelectionLowerCase == "scissors") && (computerSelectionLowerCase == "paper")) {
-        return results.textContent = "You win! Scissors beat paper!";
+        results.textContent = "You win! Scissors beat paper!", tallyP.textContent += "I";
     } else if ((playerSelectionLowerCase == "scissors") && (computerSelectionLowerCase == "rock")) {
-        return results.textContent = "You lose! Rock beats scissors!";
+        results.textContent = "You lose! Rock beats scissors!", tallyC.textContent += "I";
     } else if ((playerSelectionLowerCase == "scissors") && (computerSelectionLowerCase == "scissors")) {
-        return results.textContent = "A draw! Try again.";
+        results.textContent = "A draw! Try again.";
     } else {return results.textContent = "Not a valid input! Parameter values must be rock, paper, or scissors.";}
 
 }
+
 
 
 }
