a4bd5202617ddbdd80a88db6e69822492a06cf05
Taylor Barinka <barinkataylor@gmail.com>
2022-07-13 13:02:40 -0400

    add playerScore & computerScore internal tally

--- external.js
+++ external.js
@@ -1,20 +1,17 @@
 
-const results = document.querySelector('#round-results');
-const tallyC = document.querySelector('#tally-comp');
-const tallyP = document.querySelector('#tally-play');
-
-window.onload=function() {
-
-    //button variables
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 
-    //other variables
-const results = document.querySelector('#round-results');
+const results = document.getElementById('round-results');
+const tallyC = document.getElementById('tally-comp');
+const tallyP = document.getElementById('tally-play');
 
+let computerScore = 0;
+let playerScore = 0;
 
-    
+window.onload=function() {
+
     //button event listeners
 rock.addEventListener('click', ()=>  {
     playRound('rock');
@@ -25,6 +22,16 @@ paper.addEventListener('click', ()=>  {
 scissors.addEventListener('click', ()=>  {
     playRound('scissors');
 });
+
+
+
+//Display the running score, and announce a winner of the game once one player reaches 5 points.
+
+//1X. Create a loop that plays five rounds.
+//2X. At the end of each round, print the result using this formula: [element].textContent += "<added text>";
+//3. At the end of five rounds, print the winner. Print "Play again?" vel sim.
+//4. Reset after five turns.
+
 
 
 function computerPlay() {
@@ -38,38 +45,30 @@ function computerPlay() {
     }
 }
 
-//Display the running score, and announce a winner of the game once one player reaches 5 points.
-
-//1X. Create a loop that plays five rounds.
-//2. At the end of each round, print the result using this formula: [element].textContent += "<added text>";
-//3. At the end of five rounds, print the winner. Print "Play again?" vel sim.
-//4. Reset after five turns.
-
-
-
 function playRound(variable) {
     let playerSelectionLowerCase = variable.toLowerCase();
     let computerSelectionLowerCase = computerPlay().toLowerCase();
-    
+
     if ((playerSelectionLowerCase == "rock") && (computerSelectionLowerCase == "paper")) {
-        results.textContent = "You lose! Paper beats rock!", tallyC.textContent += "I";
+        results.textContent = "You lose! Paper beats rock!", tallyC.textContent += "I", computerScore += 1;
     } else if ((playerSelectionLowerCase == "rock") && (computerSelectionLowerCase == "rock")) {
         results.textContent = "A draw! Try again!";
     } else if ((playerSelectionLowerCase == "rock") && (computerSelectionLowerCase == "scissors")) {
-        results.textContent = "You win! Rock beats Scissors!", tallyP.textContent += "I";
+        results.textContent = "You win! Rock beats Scissors!", tallyP.textContent += "I", playerScore += 1;
     } else if ((playerSelectionLowerCase == "paper") && (computerSelectionLowerCase == "paper")) {
         results.textContent = "A draw! Try again!"
     } else if ((playerSelectionLowerCase == "paper") && (computerSelectionLowerCase == "rock")) {
-        results.textContent = "You win! Paper beats rock!", tallyP.textContent += "I";
+        results.textContent = "You win! Paper beats rock!", tallyP.textContent += "I", playerScore += 1;
     } else if ((playerSelectionLowerCase == "paper") && (computerSelectionLowerCase == "scissors")) {
-        results.textContent = "You lose! Scissors beat paper!", tallyC.textContent += "I"
+        results.textContent = "You lose! Scissors beat paper!", tallyC.textContent += "I", computerScore += 1;
     } else if ((playerSelectionLowerCase == "scissors") && (computerSelectionLowerCase == "paper")) {
-        results.textContent = "You win! Scissors beat paper!", tallyP.textContent += "I";
+        results.textContent = "You win! Scissors beat paper!", tallyP.textContent += "I", playerScore += 1;
     } else if ((playerSelectionLowerCase == "scissors") && (computerSelectionLowerCase == "rock")) {
-        results.textContent = "You lose! Rock beats scissors!", tallyC.textContent += "I";
+        results.textContent = "You lose! Rock beats scissors!", tallyC.textContent += "I", computerScore += 1;
     } else if ((playerSelectionLowerCase == "scissors") && (computerSelectionLowerCase == "scissors")) {
         results.textContent = "A draw! Try again.";
     } else {return results.textContent = "Not a valid input! Parameter values must be rock, paper, or scissors.";}
+
 
 }
 
